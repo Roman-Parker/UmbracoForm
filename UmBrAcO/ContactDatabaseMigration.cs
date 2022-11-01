e84b33e6b99f018aa9fb130047164a27be435491
Roman Parker <roman@parker.pm>
2022-11-01 09:43:05 +0000

    init

---
+++ UmBrAcO/ContactDatabaseMigration.cs
@@ -0,0 +1,106 @@
+﻿using NPoco;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.Migrations;
+using Umbraco.Cms.Core.Scoping;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Infrastructure.Migrations.Upgrade;
+using Umbraco.Cms.Infrastructure.Migrations;
+using Umbraco.Cms.Infrastructure.Persistence.DatabaseAnnotations;
+
+
+
+namespace UmBrAcO
+{
+    public class BlogCommentsComposer : ComponentComposer<ContactDatabaseMigration>, IComposer
+    {
+    }
+
+    public class ContactDatabaseMigration : IComponent
+    {
+        private readonly ICoreScopeProvider _coreScopeProvider;
+        private readonly IMigrationPlanExecutor _migrationPlanExecutor;
+        private readonly IKeyValueService _keyValueService;
+        private readonly IRuntimeState _runtimeState;
+
+        public ContactDatabaseMigration(
+            ICoreScopeProvider coreScopeProvider,
+            IMigrationPlanExecutor migrationPlanExecutor,
+            IKeyValueService keyValueService,
+            IRuntimeState runtimeState)
+        {
+            _coreScopeProvider = coreScopeProvider;
+            _migrationPlanExecutor = migrationPlanExecutor;
+            _keyValueService = keyValueService;
+            _runtimeState = runtimeState;
+        }
+
+        public void Initialize()
+        {
+            if (_runtimeState.Level < RuntimeLevel.Run)
+            {
+                return;
+            }
+
+            // Create a migration plan for a specific project/feature
+            // We can then track that latest migration state/step for this project/feature
+            var migrationPlan = new MigrationPlan("BlogComments");
+
+            // This is the steps we need to take
+            // Each step in the migration adds a unique value
+            migrationPlan.From(string.Empty)
+                .To<AddCommentsTable>("blogcomments-db");
+
+            // Go and upgrade our site (Will check if it needs to do the work or not)
+            // Based on the current/latest step
+            var upgrader = new Upgrader(migrationPlan);
+            upgrader.Execute(_migrationPlanExecutor, _coreScopeProvider, _keyValueService);
+        }
+
+        public void Terminate()
+        {
+        }
+    }
+
+    public class AddCommentsTable : MigrationBase
+    {
+        public AddCommentsTable(IMigrationContext context) : base(context)
+        {
+        }
+        protected override void Migrate()
+        {
+            Logger.LogDebug("Running migration {MigrationStep}", "AddCommentsTable");
+
+            // Lots of methods available in the MigrationBase class - discover with this.
+            if (TableExists("BlogComments") == false)
+            {
+                Create.Table<BlogCommentSchema>().Do();
+            }
+            else
+            {
+                Logger.LogDebug("The database table {DbTable} already exists, skipping", "BlogComments");
+            }
+        }
+
+        [TableName("BlogComments")]
+        [PrimaryKey("Id", AutoIncrement = true)]
+        [ExplicitColumns]
+        public class BlogCommentSchema
+        {
+            [PrimaryKeyColumn(AutoIncrement = true, IdentitySeed = 1)]
+            [Column("Id")]
+            public int Id { get; set; }
+
+            [Column("Name")]
+            public string Name { get; set; }
+
+            [Column("Email")]
+            public string Email { get; set; }
+
+
+            [Column("Message")]
+            [SpecialDbType(SpecialDbTypes.NVARCHARMAX)]
+            public string Message { get; set; }
+        }
+    }
+}
---
+++ UmBrAcO/ContactSurfaceController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Logging;
+using Umbraco.Cms.Core.Routing;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Infrastructure.Persistence;
+using Umbraco.Cms.Web.Website.Controllers;
+
+
+namespace UmBrAcO
+{
+    public class ContactDto
+    {
+        string name { get; set; }
+        string email { get; set; }
+        public string message { get; set; }
+
+
+    }
+    public class ContactSurfaceController : SurfaceController
+    {
+        public ContactSurfaceController(
+            IUmbracoContextAccessor umbracoContextAccessor,
+            IUmbracoDatabaseFactory databaseFactory,
+            ServiceContext services,
+            AppCaches appCaches,
+            IProfilingLogger profilingLogger,
+            IPublishedUrlProvider publishedUrlProvider)
+            : base(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
+        {
+        }
+
+        public IActionResult contactSubmit(ContactDto contactForm)
+        {
+
+        }
+
+    }
+}
